7f2b14620e5499e6bfb8393f1ac09479ae54482f
Stricted <info@stricted.net>
2017-04-16 20:15:42 +0200

    add updater overlay

--- product.mk
+++ product.mk
@@ -2,3 +2,9 @@
 PRODUCT_PACKAGES += \
     ThemeInterfacer \
     projekt.substratum.theme.xml
+
+# Add overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+PRODUCT_PROPERTY_OVERRIDES := \
+    ro.opa.eligible_device=true
