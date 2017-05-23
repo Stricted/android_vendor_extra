fe710e7e60d92eb125370772822f52e2744de2d3
Stricted <info@stricted.net>
2017-05-23 19:40:09 +0200

    include OMS only if WITH_OMS is true

--- product.mk
+++ product.mk
@@ -1,7 +1,9 @@
+ifeq ($(WITH_OMS),true)
 # ThemeInterfacer
 PRODUCT_PACKAGES += \
     ThemeInterfacer \
     projekt.substratum.theme.xml
+endif
 
 # Add overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
