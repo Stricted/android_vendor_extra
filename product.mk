f97b5f571f9758a811be2fff33e23ce5da2dcd07
Stricted <info@stricted.net>
2018-04-12 05:14:01 +0200

    add overlays
    Change-Id: Ie3bc790320001053b23207b3244f7c305b169e1d

--- product.mk
+++ product.mk
@@ -9,5 +9,8 @@ PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/certs/releasekey
 PRODUCT_EXTRA_RECOVERY_KEYS := vendor/certs/releasekey
 endif
 
+# Overlays
+PRODUCT_PACKAGE_OVERLAYS += vendor/extra/overlay
+
 # Use own updater
 PRODUCT_PROPERTY_OVERRIDES += lineage.updater.uri=https://lineage.stricted.net/api/v1/{device}/{type}/{incr}
