a5af84dc68b7d65cde570c7b03bbde4135611f17
Stricted <info@stricted.net>
2018-05-04 23:34:17 +0200

    set cm.updater.uri property
    Change-Id: Ifc1ec37485b7555ca3ff7b60611d927e2ad8bd94

--- product.mk
+++ product.mk
@@ -8,5 +8,8 @@ endif
 # Add overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-PRODUCT_PROPERTY_OVERRIDES := \
+PRODUCT_PROPERTY_OVERRIDES += \
+    cm.updater.uri=https://lineage.stricted.net/api/v1/{device}/{type}/{incr}
+
+PRODUCT_PROPERTY_OVERRIDES += \
     ro.opa.eligible_device=true
