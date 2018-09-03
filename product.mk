482eac1b0f1eef242b84a18a56d8864769fc3dd6
Stricted <info@stricted.net>
2018-09-03 10:46:39 +0200

    Copy adb public keys to recovery ramdisk
    Change-Id: Id8614d74bc628ecb40537d1998240d70f3084047

--- product.mk
+++ product.mk
@@ -19,6 +19,10 @@ $(call inherit-product, vendor/opengapps/build/opengapps-packages.mk)
 
 endif
 
+# Recovery ADB keys
+PRODUCT_COPY_FILES += \
+    vendor/extra/adb_keys:recovery/root/adb_keys
+
 # Overlays
 PRODUCT_PACKAGE_OVERLAYS += vendor/extra/overlay
 
